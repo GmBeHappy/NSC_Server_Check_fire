9f8c626c8d5e4b7feb95962f5dcab3a4d71b0d7d
GmBeHappy <gmplk@hotmail.com>
2021-02-07 04:54:23 +0700

    update

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import numpy as np
 
 broker = 'broker.netpie.io'
 port = 1883
-topic = "@shadow/data/update/amg/#"
-client_id = '2c1fc392-c8fa-48cd-a3a3-73d00bae7911'
-username = 'mJoqEwaWvphcXYkED4AokANJo7zpXvHt'
-password = 'rh5w58ImDcJwuKU3O8s9vwxSXx_6kJTT'
+topic = "@private/shadow/data/get/response"
+client_id = "2c1fc392-c8fa-48cd-a3a3-73d00bae7911"
+username = "mJoqEwaWvphcXYkED4AokANJo7zpXvHt"
+password = "rh5w58ImDcJwuKU3O8s9vwxSXx_6kJTT"
 
 threshold = 50
 alarm = False
@@ -23,7 +23,7 @@ def on_connect(client, userdata, flags, rc):
 # Message receiving callback
 def on_message(client, userdata, msg):
     msg.payload = msg.payload.decode("utf-8")
-    #print(msg.payload)
+    print(msg.payload)
     global check_state , ls
     if(msg.payload=="start"):
         check_state = 1
@@ -45,12 +45,13 @@ def on_message(client, userdata, msg):
         ls.clear()
 
 
-client = mqtt.Client()
+client = mqtt.Client(client_id)
 
 # Specify callback function
+client.username_pw_set(username, password)
 client.on_connect = on_connect
 client.on_message = on_message
-client.username_pw_set(username, password)
+
 
 # Establish a connection
 client.connect(broker, port, 60)
